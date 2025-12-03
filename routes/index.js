a7efd2e50abf96b11d5dbd205c684fdb3b43ba93
juin <raiawa1212@gmail.com>
2025-12-03 16:00:30 +0900

    refactor: simplify user authentication logic using early return

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
-
-// routes/index.js 임시 테스트용
 const { isAuthenticated } = require('../middlewares/authMiddleware');
-router.get('/mypage', isAuthenticated, (req, res) => {
-    res.send('여기는 로그인한 사람만 보는 마이페이지입니다.');
-});
 
 router.get('/', mainController.getHomePage);
 
+router.get('/mypage', isAuthenticated, (req, res)=>{
+  res.send('여기는 로그인한 사람만 볼수있는 마이페이지임');
+})
 
-
-module.exports = router
+module.exports = router;
